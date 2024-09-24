15fa848ebb174fbcae8094c0b8021ba6b42d5230
rushomrong <rushomarak@gmail.com>
2024-09-25 03:26:48 +0600

    updated changes

--- js/active.js
+++ js/active.js
@@ -18,37 +18,4 @@ document.addEventListener("DOMContentLoaded", function () {
       donationButton.style.backgroundColor = "";
     }
   }
-
-  const initialBalance = 5500;
-  //amount retriving for top button balance and card balance
-  // Set initial balances in localStorage if not already set
-  if (!localStorage.getItem("totalBalance")) {
-    localStorage.setItem("totalBalance", initialBalance);
-    localStorage.setItem("nkhali-balance", 0);
-    localStorage.setItem("feni-balance", 0);
-  }
-
-  // Retrieve and apply balances from sessionStorage if available
-  let totalBalance =
-    sessionStorage.getItem("totalBalance") ||
-    localStorage.getItem("totalBalance");
-  let nkhaliBalance =
-    sessionStorage.getItem("nkhali-balance") ||
-    localStorage.getItem("nkhali-balance");
-  let feniBalance =
-    sessionStorage.getItem("feni-balance") ||
-    localStorage.getItem("feni-balance");
-
-  // Update DOM
-  document.getElementById("current-balance").innerText =
-    parseFloat(totalBalance).toFixed(0);
-  document.getElementById("nkhali-balance").innerText =
-    parseFloat(nkhaliBalance).toFixed(0);
-  document.getElementById("feni-balance").innerText =
-    parseFloat(feniBalance).toFixed(0);
-
-  window.addEventListener("beforeunload", function () {
-    // Clear sessionStorage on page reload to reset balances
-    sessionStorage.clear();
-  });
 });
